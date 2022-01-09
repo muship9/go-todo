0300866df2604aada4dd204ef4ccdc66d945101d
shinp09 <57585657+shinp09@users.noreply.github.com>
2022-01-09 23:52:53 +0900

    feat: todos.go

--- app/models/base.go
+++ app/models/base.go
@@ -31,25 +31,21 @@ func init() {
 		log.Fatalln(err)
 	}
 
-	// userstableの作成
 	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		uuid STRING NOT NULL UNIQUE,
 		name STRING,
 		email STRING,
 		password STRING,
-		created_at DATETIME
-	)`, tableNameUser)
+		created_at DATETIME)`, tableNameUser)
 
 	Db.Exec(cmdU)
 
-	// todostableの作成
 	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		content TEXT,
-		user_id INTEGER
-		created_at DATETIME
-	)`, tableNameTodo)
+		user_id INTEGER,
+		created_at DATETIME)`, tableNameTodo)
 
 	Db.Exec(cmdT)
 }
--- main.go
+++ main.go
@@ -6,34 +6,30 @@ import (
 )
 
 func main() {
-	//fmt.Println(config.Config.Port)
-	//fmt.Println(config.Config.SQLDriver)
-	//fmt.Println(config.Config.DbName)
-	//fmt.Println(config.Config.LogFile)
-	//
-	//log.Println("test")
-
 	fmt.Println(models.Db)
 
 	//// userのポインタを代入
-	//u := &models.User{}
-	//u.Name = "test"
-	//u.Email = "test@example.com"ˆ
-	//u.PassWord = "testtest"
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
+	//u, _ := models.GetUser(1)
 	//fmt.Println(u)
 	//
-	//u.CreateUser()
+	//u.Name = "Test2"
+	//u.Email = "test2@example.com"
+	//u.UpdateUser()
+	//u, _ = models.GetUser(1)
+	//fmt.Println(u)
+	//
+	//u.DeleteUser()
+	//u, _ = models.GetUser(1)
+	//fmt.Println(u)
 
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 }
