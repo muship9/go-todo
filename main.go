5ee915957d308606b9738f9c563b7fc05152a276
shinp09 <57585657+shinp09@users.noreply.github.com>
2022-01-10 00:10:10 +0900

    feat: 複数のtodoを取得

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ func main() {
 	fmt.Println(models.Db)
 
 	//// userのポインタを代入
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
-
-	u.CreateUser()
+	//u := &models.User{}
+	//u.Name = "test"
+	//u.Email = "test@example.com"
+	//u.PassWord = "testtest"
+	//fmt.Println(u)
+	//
+	//u.CreateUser()
 
 	//u, _ := models.GetUser(1)
 	//fmt.Println(u)
@@ -30,6 +30,18 @@ func main() {
 	//u, _ = models.GetUser(1)
 	//fmt.Println(u)
 
+	//user, _ := models.GetUser(2)
+	//user.CreateTodo("First Todo")
+
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
+
 	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
+	user.CreateTodo("Second Todo")
+
+	todos, _ := models.GetTodos()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
+
 }
